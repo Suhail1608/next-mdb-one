5ef41e8f790d39ada33196d33e849fa82b435b4d
Suhail1608 <suhailbasha1608@gmail.com>
2024-01-08 15:01:50 +0530

    update

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -3,13 +3,7 @@ import mongoose from "mongoose";
 const connectMongo = async (uri) => {
     const db = mongoose.createConnection(uri)
     return db
-    try {
-        await mongoose.connect(uri);
-        // Optionally, you can listen for other events like 'reconnected', 'close', etc.
-
-    } catch (error) {
-        console.error('Error creating MongoDB connection:', error);
-    }
+    
 }
 
 export const todoConn = await connectMongo(process.env.MONGO_URI);
